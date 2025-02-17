c6f96463653000fe5e9c902e81884e582e8ddc58
Jagan-Konda <jagankonda83@gmail.com>
2025-02-17 20:58:56 +0530

    twenty

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const dbPath = path.join(__dirname, './tasksTracker.db')
 const app = express()
 app.use(express.json())
 
-const allowedOrigins = ['http://localhost:3000',];
+const allowedOrigins = ['http://localhost:3000', 'https://taskstracker459.netlify.app/login'];
 
 // CORS options
 const corsOptions = {
